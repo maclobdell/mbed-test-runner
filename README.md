989eba9fcc1fe45c89d9492262d7a33d42a55401
maclobdell <maclobdell@arm.com>
2018-05-14 09:18:06 -0500

    update readme, update script to handle process fails

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # simple_mbed_test_runner
+
+This is a simple way to run mbed os tests on several boards.  You kick off a test run, go have coffee or lunch.  When you get back you'll see what tests passed or failed for each connected board for each compiler.  
+
+It is optimized for simplicity, not flexibility or performance.  
+
+Its basically a wrapper around the 'mbed test' command to perform the following functions.
+1) run tests on all boards connected to the machine
+2) run tests using all compiler toolchain options
+
+Future Arguments
+(These aren't implemented yet)
+-b --branch (branch to checkout)
+-e --exclude (exclude board or toolchain)
+-n --name (pass in names of tests to run, default is all)
+-o --other_args (pass in other arguments such as reference to config files)
+
+Running tests
+    clone the mbed-os repo you want to tests
+    hook up boards to your system (plug in usb cables).
+    clone this repo on the same level as the repo you are testing from (mbed-os)
+    Execute the following command.  
+```
+    python ../simple_mbed_test_runner/run_all_tests.py
+```
+
+Known Issues
+- argument passing is not working yet (features not implemented)
+- the script currently inserts 'sudo' to run commands on linux to avoid permissions issues.  This is obviously not ideal and won't work on Windows.
+- running this tool is not ideal.  In the future I'll recommend a different procedure.   
--- run_all_tests.py
+++ run_all_tests.py
@@ -9,42 +9,56 @@ import mbed_lstools
 parser = argparse.ArgumentParser(description='Run Mbed OS tests for all toolchains')
 parser.add_argument("-m", type=str, default=["auto"], action='append', help="Target Name")
 
-#Warning: Using shell=True can be a security hazard.  Ignoring because I control the command parameters. 
+#Warning: Using shell=True can be a security hazard.  Ignoring because I control the command parameters.
 #   Used here so I can pipe output to log file using in shell command verses piping the output it with python.
 
 def main():
 
     module_name = "[run_all_tests.py] : "
     #other_args = "-n tests-* --app-config .\mbed_app.json"  #for CI Test Shield
+    #other_args = "-n tests-mbed_hal-sleep* -c"
     other_args = ""
-    toolchains = ['gcc_arm', 'arm', 'iar']            
+
+    toolchains = ['gcc_arm', 'arm', 'iar']
+    #toolchains = ['gcc_arm']
 
     args = parser.parse_args()
-    #passing in a -m argument isn't working???
+    ## TODO: Get argument parsing working.
 
     mbeds = mbed_lstools.create()
     muts = mbeds.list_mbeds(filter_function=None, unique_names=True, read_details_txt=False)
-            
+
     hr = datetime.now().hour
     min = datetime.now().minute
     sec = datetime.now().second
-    timestamp =  str(hr) + str(min) + str(sec)        
-    
+    timestamp =  str(hr) + str(min) + str(sec)
+
     print(module_name + "TIMESTAMP : " + timestamp)
-        
+
     for mut in muts:
         print(module_name + "FOUND : " + mut['platform_name'])
     for toolchain in toolchains:
-        print(module_name + "TOOLCHAIN : "  + toolchain)                                        
+        print(module_name + "TOOLCHAIN : "  + toolchain)
 
     for mut in muts:
         target = mut['platform_name']
-            
+
         for toolchain in toolchains:
-            test_command = "mbed test " + " -t " + toolchain + " -m " + target + " " + other_args + " > " + target + "_" + toolchain + "_" + timestamp + ".txt"
+            test_command = "sudo mbed test --compile " + " -t " + toolchain + " -m " + target + " " + other_args
             print(module_name + "TEST_COMMAND : " + test_command)
-            subprocess.check_call(test_command , shell=True, stderr=subprocess.STDOUT)
-            
+            try:
+                output = subprocess.check_call(test_command , shell=True, stderr=subprocess.STDOUT)
+            except Exception, e:
+                output = str(e.output)
+            print output
+
+            test_command = "sudo mbed test --run " + " -t " + toolchain + " -m " + target + " " + other_args + " --report-html " + target + "_" + toolchain + "_" + timestamp + ".html"
+            print(module_name + "TEST_COMMAND : " + test_command)
+            try:
+                output = subprocess.check_call(test_command , shell=True, stderr=subprocess.STDOUT)
+            except Exception, e:
+                output = str(e.output)
+            print output
 
 if __name__ == '__main__':
     main()
