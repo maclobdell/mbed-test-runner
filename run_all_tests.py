5acf61dad135e5adfedc222eb96aa6190772efb0
maclobdell <maclobdell@arm.com>
2018-06-06 16:12:30 -0500

    add support for specifying folder to dump results to

--- run_all_tests.py
+++ run_all_tests.py
@@ -9,6 +9,7 @@ import mbed_lstools
 parser = argparse.ArgumentParser(description='Run Mbed OS tests on all connected boards with all toolchains')
 parser.add_argument("-o", "--other_args", default="", action='store', help="Other arguments to pass as a string")  #default is string
 parser.add_argument("-d", "--dontrun", default=0, action='count', help="print commands, don't run them")  #just check if present
+parser.add_argument("-f", "--folder", default="test_output", action='store', help="Folder to dump test results to")  #default is string
 
 #Warning: Using shell=True can be a security hazard.  Ignoring because I control the command parameters.
 #   Used here so I could have the option to pipe output to log file (e.g. command > log.txt).
@@ -23,7 +24,8 @@ def main():
     args = parser.parse_args()
 
     other_args = args.other_args
-    
+    folder = args.folder
+
     mbeds = mbed_lstools.create()
     muts = mbeds.list_mbeds(filter_function=None, unique_names=True, read_details_txt=False)
 
@@ -39,6 +41,12 @@ def main():
     for toolchain in toolchains:
         print(module_name + "TOOLCHAIN : "  + toolchain)
 
+    #create directory for output folder here
+    try:
+        os.stat(folder)
+    except:
+        os.mkdir(folder)
+
     for mut in muts:
         target = mut['platform_name']
 
@@ -53,9 +61,9 @@ def main():
                     output = str(e.output)
                 print output
 
-            test_command = "sudo mbed test --run " + " -t " + toolchain + " -m " + target + " " + other_args + " --report-html " + target + "_" + toolchain + "_" + timestamp + ".html"
+            test_command = "sudo mbed test --run " + " -t " + toolchain + " -m " + target + " " + other_args + " --report-html " + "./" + folder + "/" + target + "_" + toolchain + "_" + timestamp + ".html"
             print(module_name + "TEST_COMMAND : " + test_command)
-            
+
             if args.dontrun == 0:
                 try:
                     output = subprocess.check_call(test_command , shell=True, stderr=subprocess.STDOUT)
