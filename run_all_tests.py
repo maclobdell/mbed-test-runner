49efb273dc64b781cea382c00f58387379264194
Mihail Stoyanov <wm@smode.org>
2018-09-25 19:06:30 +0100

    Complete restructure Make use of safer commands escaping (avoid "shell=True") Sequential and parallel/queued routines (the latter doesn't work due to logging)

--- run_all_tests.py
+++ run_all_tests.py
@@ -11,19 +11,24 @@ from os.path import join, abspath, dirname
 from json import load, dump
 import re
 import logging
+from multiprocessing import Pool
+from time import sleep
 from prettytable import PrettyTable
 
 parser = argparse.ArgumentParser(description='Run Mbed OS tests on all connected boards with all toolchains')
 parser.add_argument("-t", "--toolchain", default="", action='store', help="Specified toolchain(s). You can provide comma-separated list.")  #default is string
 parser.add_argument("-j", "--jobs", default=1, action='store', help="Number of jobs. Default 1.")  #default is string
 parser.add_argument("-d", "--dryrun", default=False, action='store_true', help="print commands, don't run them")  #just check if present
-parser.add_argument("-f", "--folder", default="test_output", action='store', help="Folder to dump test results to")  #default is string
+parser.add_argument("-f", "--folder", default="TEST_OUTPUT", action='store', help="Folder to dump test results to")  #default is string
 parser.add_argument("-r", "--report", default="json", action='store', help="Output format : json, html, text, xml")  #default is string
 parser.add_argument("-o", "--other_args", default="", action='store', help="Other arguments to pass as a string")  #default is string
 
 #Warning: Using shell=True can be a security hazard.  Ignoring because I control the command parameters.
 #   Used here so I could have the option to pipe output to log file (e.g. command > log.txt).
 
+class ToolException(Exception):
+    """A class representing an exception throw by the tools"""
+    pass
 
 def main():
     args = parser.parse_args()
@@ -31,6 +36,7 @@ def main():
     folder = args.folder  #folder to put the results
     report_type = args.report
     current_path = os.getcwd()
+    jobs_count = int(args.jobs)
 
     if args.toolchain:
         toolchains = args.toolchain.split(",")
@@ -51,133 +57,201 @@ def main():
         os.stat(folder)
     except:
         os.mkdir(folder)
+    report_base = folder
 
     #Set up log
-    log_file_path = current_path + "/" + folder + "/test_runner_log_" + timestamp + ".txt"
-    logging.basicConfig(filename=log_file_path, level=logging.DEBUG)
+    log_file = current_path + "/" + folder + "/test_runner_log_" + timestamp + ".txt"
+    logging.basicConfig(filename=log_file, level=logging.DEBUG)
     log = logging.getLogger("Test Runner")
     log.setLevel(logging.DEBUG)     
 
-    logger("-----------------------------------",log)
-    logger("Simple Mbed Test Runner Log",log)
-    logger("-----------------------------------",log)
-    logger("TIMESTAMP : " + timestamp,log)
+    logger("-----------------------------------", log)
+    logger("Simple Mbed Test Runner Log", log)
+    logger("-----------------------------------", log)
+    logger("TIMESTAMP : " + timestamp, log)
     for mut in muts:
-        logger("PLATFORM : " + mut['platform_name'],log)
+        logger("PLATFORM : " + mut['platform_name'], log)
     for toolchain in toolchains:
-        logger("TOOLCHAIN : "  + toolchain,log)
-    logger("Path: " + current_path,log)    
-    logger("Results Folder: " + folder,log)
-    output = subprocess.check_output("git rev-parse --abbrev-ref HEAD" , shell=True, stderr=subprocess.STDOUT) #get the branch name
+        logger("TOOLCHAIN : "  + toolchain, log)
+    logger("PATH: " + current_path, log)    
+    logger("RESULTS DIR: " + folder, log)
+    output = subprocess.check_output("git rev-parse HEAD" , shell=True, stderr=subprocess.STDOUT) #get the branch name
     mbed_ver = re.sub(r'\W+', '', output)  #remove weird characters
-    logger("Mbed OS Ver: " + mbed_ver,log)
+    logger("Mbed OS Ver: " + mbed_ver, log)
 
-    logger("Test Parameters: " + other_args,log)
-    logger("-----------------------------------",log)
+    logger("Test Parameters: " + other_args, log)
+    logger("-----------------------------------", log)
 
-    #change to the test results output directory
-    os.chdir(folder)
-
-    #create directory for the mbed version results - this is just the branch name (or maybe tag?)
-    try:
-        os.stat(mbed_ver)
-    except:
-        os.mkdir(mbed_ver)
-
-    #change to the directory that is named the same as the mbed os version  / branch or tag name
-    os.chdir(mbed_ver)
-    mbed_ver_path = os.getcwd()
-    
-    logger("Testing Started",log)
-    logger("Executing Commands...",log)
-        
+    jobs = []
     for mut in muts:
         target = mut['platform_name']
 
-        #create directory for the platform
-        try:
-            os.stat(target)
-        except:
-            os.mkdir(target)
+        job = {
+            'target': target,
+            'toolchains': toolchains,
+            'timestamp': timestamp,
+            'other_args': other_args,
+            'report_dir': os.path.join(report_base, mbed_ver, target),
+            'report_type': report_type,
+            'dryrun': args.dryrun,
+            'log': log
+        }
+        jobs.append(job)
 
-        os.chdir(target)
-        
-        #The output folder path is the name of the directory at /<test_output>/<mbed_version>/<platform>
-        output_folder_path = os.getcwd()
-        log.debug("Results Going To: " + output_folder_path)
-        
-        #Go back up to the top
-        os.chdir(current_path)
-        
-        for toolchain in toolchains:
-            test_command = "mbed test --compile " + " -t " + toolchain + " -m " + target + " " + other_args
-            logger(test_command,log)
+    logger("Testing Started...", log)
+    if jobs_count <= 1:
+        test_seq(jobs)
+    else:
+        test_queue(jobs, jobs_count)
 
-            #Set an appropriate output file name
-            if report_type == "text":
-                output_file_name = target + "_" + toolchain + "_" + timestamp + "_results.txt"
-                report_arg = "--report-text"    
-            elif report_type == "html" :
-                output_file_name = target + "_" + toolchain + "_" + timestamp + "_results.html"
-                report_arg = "--report-html"
-            elif report_type == "xml" :
-                output_file_name = target + "_" + toolchain + "_" + timestamp + "_results.xml"
-                report_arg = "--report-junit"    
+
+def test_worker(job):
+    log = job['log']
+    _rc = 255
+    report_file = None
+
+    for toolchain in job['toolchains']:
+        cmd_compile = ["mbed", "test", "--compile", "-t", toolchain, "-m", job['target']] + (job['other_args'].split(' ') if job['other_args'] else [])
+        logger("EXEC: %s" % ' '.join(cmd_compile), log)
+        if not job['dryrun']:
+            try:
+                _stdout, _stderr, _rc = run_cmd(cmd_compile)
+                log.debug("Command Output:" + str(_stdout))
+            except Exception, e:
+                log.error("Result: COMPILE COMMAND FAILED for %s %s: %s (%s)" % (toolchain, job['target'], e.args[0], e.args[1]))
+                continue
+
+        if not os.path.exists(job['report_dir']):
+            log.debug("Creating report dir %s" % job['report_dir'])
+            os.makedirs(job['report_dir'])
+
+        #Set an appropriate output file name
+        if job['report_type'] == "text":
+            report_file = job['target'] + "_" + toolchain + "_" + job['timestamp'] + "_results.txt"
+            report_arg = "--report-text"    
+        elif job['report_type'] == "html" :
+            report_file = job['target'] + "_" + toolchain + "_" + job['timestamp'] + "_results.html"
+            report_arg = "--report-html"
+        elif job['report_type'] == "xml" :
+            report_file = job['target'] + "_" + toolchain + "_" + job['timestamp'] + "_results.xml"
+            report_arg = "--report-junit"    
+        else:
+            #anytthing else, use json output
+            report_file = job['target'] + "_" + toolchain + "_" + job['timestamp'] + "_results.json"
+            report_arg = "--report-json"
+
+        cmd_test = ["mbed", "test", "--run", "-t", toolchain, "-m", job['target'], report_arg, job['report_dir'] + "/" + report_file] + (job['other_args'].split(' ') if job['other_args'] else [])
+        logger("EXEC: %s" % ' '.join(cmd_test), log)
+        if not job['dryrun']:
+            try:
+                _stdout, _stderr, _rc = run_cmd(cmd_test)
+                log.debug("Command Output:" + str(_stdout))
+            except Exception, e:
+                log.error("Result: TEST COMMAND FAILED %s %s" % (toolchain, job['target']))
+
+    return {
+        'code': _rc,
+        'report_dir': job['report_dir'],
+        'report_type': job['report_type'],
+        'report_file': report_file
+    }
+
+
+
+# Test sequentially
+def test_seq(queue):
+    for item in queue:
+        result = test_worker(item)
+
+        if result['report_type'] == "json":
+            log_test_summary(result['report_dir'], result['report_file'], log)
+
+    return True
+
+
+# Test in parallel
+def test_queue(queue, jobs_count):
+    p = Pool(processes=jobs_count)
+
+    results = []
+    for i in range(len(queue)):
+        results.append(p.apply_async(test_worker, [queue[i]]))
+    p.close()
+
+    itr = 0
+    while len(results):
+        itr += 1
+        if itr > 3600:
+            p.terminate()
+            p.join()
+            raise Exception(255, "Test did not finish in 1 hour")
+
+        sleep(1)
+        pending = 0
+        for r in results:
+            if r.ready():
+                try:
+                    result = r.get()
+                    results.remove(r)
+
+                    if result['report_type'] == "json":
+                        log_test_summary(result['report_dir'], result['report_file'], log)
+                except ToolException as err:
+                    if p._taskqueue.queue:
+                        p._taskqueue.queue.clear()
+                        sleep(1)
+                    p.terminate()
+                    p.join()
+                    raise ToolException(err)
             else:
-                #anytthing else, use json output
-                output_file_name = target + "_" + toolchain + "_" + timestamp + "_results.json"
-                report_arg = "--report-json"
+                pending += 1
+                if pending >= jobs_count:
+                    break
 
-            if not args.dryrun:
-                try:
-                    call_result = subprocess.check_call(test_command , shell=True, stderr=subprocess.STDOUT)
-                except Exception, e:
-                    call_result = str(e.output)
-                    log.error("Result: COMPILE COMMAND FAILED " + toolchain + " " + target)
-                log.debug("Command Output " + str(call_result))                
-            
-            test_command = "mbed test --run " + " -t " + toolchain + " -m " + target + " " + other_args + " " + report_arg + " " + output_folder_path + "/" + output_file_name    
-            logger(test_command,log)
-            
-            if not args.dryrun:
-                try:
-                    call_result = subprocess.check_call(test_command , shell=True, stderr=subprocess.STDOUT)
-                except Exception, e:
-                    call_result = str(e.output)
-                    log.error("Result: TEST COMMAND FAILED " + toolchain + " " + target)
-                log.debug("Command Output " + str(call_result))
+    results = None
+    p.join()
 
-                if report_type == "json":
-                    log_test_summary(output_folder_path, output_file_name, log)
-        #get back into position for the next target    
-        os.chdir(mbed_ver_path)                
-                 
+    return True
+
+
+def run_cmd(command, work_dir=None, redirect=False):
+    try:
+        process = subprocess.Popen(command, cwd=work_dir)
+        _stdout, _stderr = process.communicate()
+    except Exception as e:
+        print("[OS ERROR] Command: \"%s\" (%s) %s" % (' '.join(command), e.args[0], e.args[1]))
+        raise e
+
+    return _stdout, _stderr, process.returncode
+
+
 def logger(details, log):
     print(details)
     log.info(details)
 
-def log_test_summary(output_foler_path, output_file_name, log):    
-        #open the log file 
-        test_data_json_file = os.path.join(output_foler_path, output_file_name)    
-        with open (test_data_json_file, "r") as f:
-            test_data = json.loads(f.read()) 
-            f.close()
 
-        #create table
-        x = PrettyTable()        
-        x.field_names = ["target", "platform_name", "test suite", "result"," elapsed_time (sec)"]
+def log_test_summary(output_foler_path, report_file, log):
+    #open the log file 
+    test_data_json_file = os.path.join(output_foler_path, report_file)
+    with open (test_data_json_file, "r") as f:
+        test_data = json.loads(f.read()) 
+        f.close()
 
-        #TODO check if file valid, and results are available first
+    #create table
+    x = PrettyTable()
+    x.field_names = ["target", "platform_name", "test suite", "result"," elapsed_time (sec)"]
 
-        #read test log for test suite results, put rows in the table
-        for target_toolchain in test_data:                
-            platform, toolchain = target_toolchain.split("-")
-            target_test_data = test_data[target_toolchain]
-            for test_suite in target_test_data:
-                test_suite_data = target_test_data[test_suite]  
-                x.add_row([target_toolchain, platform, test_suite, test_suite_data.get("single_test_result", "none"),test_suite_data.get("elapsed_time", "none")])
+    #TODO check if file valid, and results are available first
 
-        logger(x, log)
+    #read test log for test suite results, put rows in the table
+    for target_toolchain in test_data:                
+        platform, toolchain = target_toolchain.split("-")
+        target_test_data = test_data[target_toolchain]
+        for test_suite in target_test_data:
+            test_suite_data = target_test_data[test_suite]  
+            x.add_row([target_toolchain, platform, test_suite, test_suite_data.get("single_test_result", "none"),test_suite_data.get("elapsed_time", "none")])
+
+    logger(x, log)
 
 if __name__ == '__main__':
     main()
